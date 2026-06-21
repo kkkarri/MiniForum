354b2f7f61f10c458f3900e618c2a016edd96ea2
kkka.rri <160507430+kkkarri@users.noreply.github.com>
2026-06-22 00:20:40 +0300

    Add files via upload

---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics;
+
+// Прописуємо обидва варіанти просторів імен як глобальні, 
+// щоб Visual Studio бачила їх у будь-якому файлі!
+global using MiniForum.Models;
+global using MiniForum.Data;
+global using WebApplication1.Models;
+global using WebApplication1.Data;
